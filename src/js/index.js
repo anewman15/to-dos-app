a58df67217d279b6908997b6a7d917b44f3149a7
Abdullah Numan <anewman15@hotmail.com>
2020-12-18 15:08:45 +0600

    Remove unused variables

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,9 @@
-import { displayProjectList, displayProjectContent } from './dom_module';
+import { displayProjectList } from './dom_module';
 import * as EventListeners from './event_listeners';
 import * as Storage from './storage';
 
 Storage.initiateProjectsStore();
 displayProjectList();
-// displayProjectContent(Storage.getProjects(), 0);
 
 EventListeners.projectFormEventListener();
 EventListeners.projectLinkEventListener();
